dd2e59ce0f585642c0689df920f5355ce02d4301
Tony Lownds <tony@tintup.com>
2017-01-08 16:17:52 -0800

    Simple integration test

--- pullup.js
+++ pullup.js
@@ -1,15 +1,13 @@
 module.exports = function ({emitter, state, docker}) {
     emitter.on('push', ({tag}) => {
-        var containerInfo = state.containers[tag];
-        if (!containerInfo) return console.log('Container not running:', tag);
+        var info = state.containers[tag];
+        if (!info) return console.log('Container not running:', tag);
         state.tags
         .concat(state.scannedTags || [])
         .filter((tagSpec) => tagsMatch(tag, tagSpec))
         .forEach((tagSpec) => {
                 state.busy++;
-                pullUp(docker, tag, containerInfo, () => {
-                    state.busy--;
-                })
+                pullUp(docker, tag, info).then(() => state.busy--);
             });
     });
 }
@@ -21,39 +19,45 @@ function tagsMatch(tag, tagSpec) {
 }
 
 function pullUp(docker, repoTag, containerInfo, cb) {
-    console.log('pullUp:', repoTag, containerInfo);
-    if (!containerInfo.id) return error('Error getting tag');
-    var oldC = docker.getContainer(containerInfo.id);
-    var newC;
-    docker.pull(repoTag, (err, stream) => {
-      docker.modem.followProgress(stream, createContainer);
-    });
-    function createContainer() {
-        var info = oldC.inspect((err, info) => {
-            info.Config.Hostname = ''; // copied from conduit... might need to be validated?
-            docker.createContainer(info.Config, (err, container) => {
-                if (err) return error('Error creating new container, aborting', err);
-                newC = container;
-                stopOldContainer();
+    return new Promise((resolve, reject) => {
+        console.log('pullUp:', repoTag, containerInfo);
+        if (!containerInfo.id) return error('Error getting tag');
+        var oldC = docker.getContainer(containerInfo.id);
+        var newC;
+        docker.pull(repoTag, (err, stream) => {
+            docker.modem.followProgress(stream, createContainer);
+        });
+
+        function createContainer() {
+            var info = oldC.inspect((err, info) => {
+                info.Config.Hostname = ''; // copied from conduit... might need to be validated?
+                docker.createContainer(info.Config, (err, container) => {
+                    if (err) return error('Error creating new container, aborting', err);
+                    newC = container;
+                    stopOldContainer();
+                });
             });
-        });
-    }
-    function stopOldContainer() {
-        oldC.stop(() => {
-            startNewContainer();
-            oldC.remove(() => {console.log('Removed container')});
-        });
-    }
-    function startNewContainer() {
-        newC.start((err, container) => {
-            if (err) return error('FATAL: Pullup has killed your website! Could not start container: '+repoTag, err);
-            console.log('Container started: '+repoTag);
-            cb(null);  // success!
-        });
-    }
-    function error(message, err) {
-        console.log(message);
-        console.log(err);
-        cb(err);
-    }
+        }
+        function stopOldContainer() {
+            oldC.stop(() => {
+                startNewContainer();
+                oldC.remove(() => {console.log('Removed container')});
+            });
+        }
+        function startNewContainer() {
+            newC.start((err, container) => {
+                if (err) return error('FATAL: Pullup has killed your website! Could not start container: '+repoTag, err);
+                console.log('Container started: '+repoTag);
+                resolve();  // success!
+            });
+        }
+        function error(message, err) {
+            console.log(message);
+            console.log(err);
+            reject(err);
+        }
+    });
 }
+
+// For testing purposes, expose internal pullUp function
+module.exports.pullUp = pullUp;
---
+++ test/pullup.spec.js
@@ -0,0 +1,14 @@
+const pshell = require('pshell');
+pshell.options.echoCommand = true;
+pshell.options.captureOutput = true;
+
+const pullup = require('../pullup');
+const Docker = require('dockerode');
+
+test('this runs', async () => {
+  await pshell('cd test && docker-compose up -d');
+  var res = await pshell('cd test && docker-compose ps -q nginx');
+  var id = res.stdout.trim();
+  var info = await pullup.pullUp(new Docker(), 'localhost:5000/nginx', {id});
+  expect(info).not.toBeNull();
+});
