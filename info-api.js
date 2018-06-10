3e189730fa7d120fa163fe4245f2166cbde0e2dd
Tony Lownds <tony@lownds.com>
2018-06-10 09:26:16 -0700

    Don't expose unneeded info

--- info-api.js
+++ info-api.js
@@ -2,6 +2,6 @@ module.exports = function ({app, state}) {
     state.info = {};
     app.get('/', (req, res) => {
         state.info.mem = process.memoryUsage();
-        res.json(state);
+        res.json(state.info);
     });
 }
