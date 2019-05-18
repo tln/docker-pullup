d1e3048419a1629429f1ca183a6dd80ad6dee679
Tony Lownds <tony@lownds.com>
2019-05-17 17:21:49 -0700

    Misc testing tweaks

--- test/swarm_test.sh
+++ test/swarm_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 mkdir -p test/gen
-(cat test/Dockerfile; echo 'RUN echo' `date`) > test/gen/Dockerfile
+(cat test/Dockerfile; echo 'RUN echo' `date` '> /usr/share/nginx/html/index.html') > test/gen/Dockerfile
 docker build -t localhost:5000/nginx:latest test/gen
 docker push localhost:5000/nginx:latest
